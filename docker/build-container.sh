13288f88215edeff0755bd132f9caf0f7efae2c6
Euan Harris <euan.harris@citrix.com>
2016-06-17 17:11:08 +0100

    docker: Don't use the cache, to avoid stale rebuilds
    Signed-off-by: Euan Harris <euan.harris@citrix.com>

--- docker/build-container.sh
+++ docker/build-container.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -xe
 
-docker build -t planex-release:0.7.3 --force-rm=true -f Dockerfile.release .
-docker build -t planex-master --force-rm=true -f Dockerfile.master .
-docker build -t planex-unstable --force-rm=true -f Dockerfile.unstable .
+docker build -t planex-release:0.7.3 --no-cache --rm=true --force-rm -f Dockerfile.release .
+docker build -t planex-master --no-cache --rm=true --force-rm -f Dockerfile.master .
+docker build -t planex-unstable --no-cache --rm=true --force-rm -f Dockerfile.unstable .
